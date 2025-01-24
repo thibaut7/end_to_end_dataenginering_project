d663e79b20982948348d279b0f57904b0810e8d8
thibaut7 <thibautmcdonald@gmail.com>
2025-01-24 07:59:22 +0000

    first success of streaming the data in kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import uuid
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 import requests
 import json
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
 
 default_args ={
-    'owner': 'thibaut7',
-    'start_date': datetime(2024, 11, 8, 18, 3, 00)
+    'owner': 'admin',
+    'start_date': datetime(2025, 1, 23)  # Start immediately
 }
 
 def get_data():
@@ -21,7 +21,6 @@ def get_data():
         data['firstname'] = res['name']['first']
         data['lastname'] = res ['name']['last']
         data['address'] = str(res['location']['street']['number']) + ' ' + str(res['location']['street']['name']) + ', ' + str( res['location']['city'])+ ', ' + str(res['location']['state']) + ', '  +  str(res['location']['country'])
-        
         data['postcode'] = res['location']['postcode']
         data['email'] = res['email']
         data['username'] = res['login']['username']
@@ -36,32 +35,62 @@ def get_data():
 
 def stream_data():
     import json
-    from kafka import KafkaProducer
+    from confluent_kafka import Producer
     import time
     import logging
+    from time import sleep
 
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    current_time = time.time()
-    
-    while True:
-        if time.time() > current_time + 60:
-            break
+    # Producer configuration with Docker network settings
+    config = {
+        'bootstrap.servers': 'kafka1:29092,kafka2:29093',  # Docker internal network addresses
+        'client.id': 'python-producer',
+        'retries': 5,
+        'retry.backoff.ms': 1000
+    }
+
+    # Create Producer instance with retry logic
+    max_retries = 5
+    for attempt in range(max_retries):
         try:
-            res = get_data()
-            producer.send('users_created', json.dumps(res).encode('utf-8'))
-            producer.flush()  # Ensure all messages are sent
+            producer = Producer(config)
+            break
         except Exception as e:
-            logging.error('An error occured: {e}')
-            continue
-
+            if attempt == max_retries - 1:
+                raise
+            print(f"Connection attempt {attempt + 1} failed, retrying...")
+            sleep(2)
+    
+    try:
+        while True:
+            # Get data from API
+            data = get_data()
+            
+            # Produce message
+            producer.produce(
+                topic='user_data',
+                key=str(time.time()),
+                value=json.dumps(data),
+                callback=delivery_report
+            )
+            
+            # Flush producer queue
+            producer.poll(0)
+            time.sleep(1)
+            
+    except KeyboardInterrupt:
+        print('Interrupted by user')
+    finally:
+        producer.flush(timeout=5)
+        
 with DAG('user_automation',
         default_args=default_args,
-        schedule='@hourly',
+        schedule_interval=timedelta(minutes=2),  # Run every 2 minutes
         catchup=False) as dag:
         
     streaming_task = PythonOperator(
         task_id='stream_data_from_api',
         python_callable=stream_data
-        )
-
+        )    
+# if __name__ == '__main__':
+#     stream_data()
 
